18e81e803282e754f9f2e8373882233eb16eb15a
Vaishnal Mali <vaishnal16305@gmail.com>
2025-01-24 22:34:57 +0530

    streamlit deployment done

--- frontend/src/pages/MockInterviewpPrep.jsx
+++ frontend/src/pages/MockInterviewpPrep.jsx
@@ -5,7 +5,7 @@ export default function MockInterviewPrep() {
     <>
       <div className="w-full h-screen">
         <iframe
-          src="http://localhost:8502"
+          src="https://vaishnal16-streamlit-app-main-ev0jwf.streamlit.app/?embedded=true"
           className="w-full h-full"
           title="Streamlit UI"
         ></iframe>
